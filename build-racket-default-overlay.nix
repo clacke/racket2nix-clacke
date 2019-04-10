58613dbf8078c77b2506e1f1fc63076f815f1623
Claes Wallin (韋嘉誠) <claes.wallin@greatsinodevelopment.com>
2019-04-10 22:47:49 +0800

    WIP: gui-lib: lib hook

--- build-racket-default-overlay.nix
+++ build-racket-default-overlay.nix
@@ -10,7 +10,17 @@ lib.optionalAttrs (super ? "deinprogramm-signature+htdp-lib" && super ? "icons")
   "deinprogramm-signature+htdp-lib" = super."deinprogramm-signature+htdp-lib".overrideRacketDerivation (oldAttrs: { racketBuildInputs = oldAttrs.racketBuildInputs ++ [ self.icons ]; });
 } //
 lib.optionalAttrs (super ? "gui-lib" && super ? "icons") {
-  gui-lib = super.gui-lib.overrideRacketDerivation (oldAttrs: { racketBuildInputs = oldAttrs.racketBuildInputs ++ [ self.icons ]; });
+  gui-lib = super.gui-lib.overrideRacketDerivation (oldAttrs: {
+    racketBuildInputs = oldAttrs.racketBuildInputs ++ [ self.icons ];
+    libPath = lib.makeLibraryPath [ super.pkgs.xlibs.libX11 ];
+    setupHook = builtins.toFile "setup-hook.sh" ''
+      addRacketGuiLibLibs() {
+        LD_LIBRARY_PATH=''${LD_LIBRARY_PATH:-}''${LD_LIBRARY_PATH:+:}@libPath@
+      }
+
+      addEnvHooks "$targetOffset" addRacketGuiLibLibs
+    '';
+  });
 } //
 lib.optionalAttrs (super ? "htdp-lib" && super ? "icons") {
   htdp-lib = super.htdp-lib.overrideRacketDerivation (oldAttrs: { racketBuildInputs = oldAttrs.racketBuildInputs ++ [ self.icons ]; });
