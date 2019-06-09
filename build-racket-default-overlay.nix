d7b98dac953ed95e0c6cf2e92f041b0d5ffc9e1b
Claes Wallin (韋嘉誠) <claes.wallin@greatsinodevelopment.com>
2019-06-09 13:52:29 +0800

    Problem: raart: Undeclared dependencies
    Visible when depending on raart from another package.
    
        raco setup: --- summary of package problems ---
        raco setup: undeclared dependency detected
        raco setup:   for package: raart
        raco setup:   on packages:
        raco setup:    gui-lib
        raco setup:    htdp-lib
        raco setup:    pict-lib
        raco setup:    plot-gui-lib
        raco setup:    plot-lib
    
    Solution: Add dependencies.

--- build-racket-default-overlay.nix
+++ build-racket-default-overlay.nix
@@ -18,6 +18,22 @@ lib.optionalAttrs (super ? "gui-lib" && super ? "icons") {
 lib.optionalAttrs (super ? "htdp-lib" && super ? "icons") {
   htdp-lib = super.htdp-lib.overrideRacketDerivation (oldAttrs: { racketBuildInputs = oldAttrs.racketBuildInputs ++ [ self.icons ]; });
 } //
+lib.optionalAttrs (super ? "raart") { raart = super.raart.overrideAttrs (oldAttrs: { patches = [ (builtins.toFile "deps.patch" ''
+--- a/raart/info.rkt
++++ b/raart/info.rkt
+@@ -4,6 +4,11 @@
+                "unix-signals"
+                "reprovide-lang"
+                "ansi"
++               "gui-lib"
++               "htdp-lib"
++               "pict-lib"
++               "plot-gui-lib"
++               "plot-lib"
+                "struct-define"
+                "base"))
+ (define build-deps '("sandbox-lib"
+'') ]; }); } //
 lib.optionalAttrs (super ? "compatibility+compatibility-doc+data-doc+db-doc+distributed-p...") {
   "compatibility+compatibility-doc+data-doc+db-doc+distributed-p..." = super."compatibility+compatibility-doc+data-doc+db-doc+distributed-p...".overrideAttrs (oldAttrs: {
   buildInputs = oldAttrs.buildInputs or [] ++ builtins.attrValues {
